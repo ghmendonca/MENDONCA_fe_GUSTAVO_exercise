8ab27d9c6797ffdfc5709ef4d28df8f306f930c3
Gustavo <gustavohm.mendonca@gmail.com>
2023-02-25 19:39:22 -0300

    fix lint issue

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,4 @@
-import { Team, TeamOverview, UserData } from 'types';
-
-
+import {Team, TeamOverview, UserData} from 'types';
 
 const getData = async (path = '') => {
     const url = `${process.env.REACT_APP_API_BASE_URL}/${path}`;
