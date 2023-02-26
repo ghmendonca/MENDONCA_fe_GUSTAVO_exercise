e9e48ae95b34c3b318f6ead816820e9ebce19fdc
Gustavo <gustavohm.mendonca@gmail.com>
2023-02-26 17:30:28 -0300

    add search field for teams

--- src/pages/Teams/index.tsx
+++ src/pages/Teams/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Input from 'components/Input';
 import Header from '../../components/Header';
 import List from '../../components/List';
 import {Container} from '../../components/GlobalComponents';
 import {useComponentState} from './state';
 
 const Teams = () => {
-    const {loading, teams} = useComponentState();
+    const {loading, teams, filter, onChangeFilter} = useComponentState();
 
     return (
         <Container>
             <Header title="Teams" showBackButton={false} />
+            <Input placeholder='Search by name' value={filter} onChange={onChangeFilter} />
             <List items={teams} isLoading={loading} />
         </Container>
     );
--- src/pages/Teams/state.ts
+++ src/pages/Teams/state.ts
@@ -1,9 +1,10 @@
-import {useMemo} from 'react';
+import {useCallback, useMemo, useState} from 'react';
 import {ListItem} from 'types';
 import {useQuery} from 'utils/useQuery';
 import teamsApi from '../../api/teams';
 
 export const useComponentState = () => {
+    const [filter, setFilter] = useState<string>('');
     const {data: teams, loading} = useQuery(() => teamsApi.getAll());
     
     const items: ListItem[] = useMemo(() => {
@@ -11,21 +12,29 @@ export const useComponentState = () => {
             return [];
         }
 
-        return teams.map((team) => ({
-            id: team.id,
-            url: `/team/${team.id}`,
-            columns: [
-                {
-                    key: 'Name',
-                    value: team.name,
-                },
-            ],
-            navigationProps: team,
-        }));
-    }, [teams]);
+        return teams
+            .filter((team) => filter.length === 0 || team.name.toLowerCase().startsWith(filter.toLowerCase()))
+            .map((team) => ({
+                id: team.id,
+                url: `/team/${team.id}`,
+                columns: [
+                    {
+                        key: 'Name',
+                        value: team.name,
+                    },
+                ],
+                navigationProps: team,
+            }));
+    }, [teams, filter]);
+
+    const onChangeFilter: React.ChangeEventHandler<HTMLInputElement> = useCallback((event) => {
+        setFilter(event.target.value);
+    }, [setFilter]);
 
     return {
         loading,
         teams: items,
+        filter,
+        onChangeFilter,
     };
 };
